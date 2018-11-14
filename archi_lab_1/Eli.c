07b4f0fcafbfbfb9e043cbd3e1665dc5597cb5b4
Eli Slavutsky <eli308@gmail.com>
2018-11-14 23:22:48 +0200

    ..

--- archi_lab_1/Eli.c
+++ archi_lab_1/Eli.c
@@ -8,7 +8,9 @@ void parser(int *procimg, Instruction *inst, char *exeStr)
 	while (1)
 	{
 		MemToInst(Mem[inst_num], inst); // update instruction field
-		if inst
+		if (inst->opcode == HLT)
+			break;
+		ExecuteCmd(procimg, inst, exeStr, &inst_num);
 
 	}
 }
@@ -31,53 +33,212 @@ void MemToInst(int mem, Instruction *inst)
 	inst->instruction = mem;
 }
 
-void ExecuteCmd(int *procimg, Instruction cmd, char *exeSrt)
+void ExecuteCmd(int *procimg, Instruction *cmd, char *exeStr, int *inst_num) // need to code the pc increment and the inst_num increment.
 {
-	if (cmd.opcode == ADD)
+	if (cmd->opcode == ADD)
 	{
-		if (cmd.src1 == 1 && cmd.src0 != 1)
-		{
-			*(procimg + cmd.dst) = cmd.immediate + *(procimg + cmd.src0);
-			strcpy(exeSrt, "EXEC: R[%d] = %d + %d", cmd.dst, cmd.src0, cmd.immediate);
-		}
-		if (cmd.src1 == 1 && cmd.src0 == 1)
-		{
-			*(procimg + cmd.dst) = cmd.immediate + cmd.immediate;
-			strcpy(exeSrt, "EXEC: R[%d] = %d + %d", cmd.dst, cmd.immediate, cmd.immediate);
-		}
-		if (cmd.src1 != 1 && cmd.src0 == 1)
-		{
-			*(procimg + cmd.dst) = cmd.immediate + *(procimg + cmd.src1);
-			strcpy(exeSrt, "EXEC: R[%d] = %d + %d", cmd.dst, cmd.src1, cmd.immediate);
-		}
-		if (cmd.src1 != 1 && cmd.src0 != 1)
-		{
-			*(procimg + cmd.dst) = *(procimg + cmd.src0) + *(procimg + cmd.src1);
-			strcpy(exeSrt, "EXEC: R[%d] = %d + %d", cmd.dst, cmd.src0, cmd.src0);
-		}
-		
+		AddFunc(procimg, cmd, exeStr);
+		return;
+	}
+	if (cmd->opcode == SUB)
+	{
+		SubFunc(procimg, cmd, exeStr);
+		return;
+	}
+	if (cmd->opcode == LSF)
+	{
+		LsfFunc(procimg, cmd, exeStr);
+		return;
+	}
+	if (cmd->opcode == RSF)
+	{
+		RsfFunc(procimg, cmd, exeStr);
+		return;
+	}
+	if (cmd->opcode == AND)
+	{
+		AndFunc(procimg, cmd, exeStr);
+		return;
+	}
+	if (cmd->opcode == OR)
+	{
+		OrFunc(procimg, cmd, exeStr);
+		return;
+	}
+	if (cmd->opcode == XOR)
+	{
+		XorFunc(procimg, cmd, exeStr);
+		return;
+	}
+
+
+}
+
+// exe funcs
+void AddFunc(int *procimg, Instruction *cmd, char *exeSrt)
+{
+	if (cmd->src1 == 1 && cmd->src0 != 1)
+	{
+		*(procimg + cmd->dst) = cmd->immediate + *(procimg + cmd->src0);
+		strcpy(exeSrt, "EXEC: R[%d] = %d + %d", cmd->dst, cmd->src0, cmd->immediate);
+	}
+	if (cmd->src1 == 1 && cmd->src0 == 1)
+	{
+		*(procimg + cmd->dst) = cmd->immediate + cmd->immediate;
+		strcpy(exeSrt, "EXEC: R[%d] = %d + %d", cmd->dst, cmd->immediate, cmd->immediate);
+	}
+	if (cmd->src1 != 1 && cmd->src0 == 1)
+	{
+		*(procimg + cmd->dst) = cmd->immediate + *(procimg + cmd->src1);
+		strcpy(exeSrt, "EXEC: R[%d] = %d + %d", cmd->dst, cmd->src1, cmd->immediate);
+	}
+	if (cmd->src1 != 1 && cmd->src0 != 1)
+	{
+		*(procimg + cmd->dst) = *(procimg + cmd->src0) + *(procimg + cmd->src1);
+		strcpy(exeSrt, "EXEC: R[%d] = %d + %d", cmd->dst, cmd->src0, cmd->src1);
 	}
 }
-void AddFunc(int *procimg, Instruction cmd, char *exeSrt)
+
+void SubFunc(int *procimg, Instruction *cmd, char *exeSrt)
 {
-	if (cmd.src1 == 1 && cmd.src0 != 1)
+	if (cmd->src1 == 1 && cmd->src0 != 1)
 	{
-		*(procimg + cmd.dst) = cmd.immediate + *(procimg + cmd.src0);
-		strcpy(exeSrt, "EXEC: R[%d] = %d + %d", cmd.dst, cmd.src0, cmd.immediate);
+		*(procimg + cmd->dst) = *(procimg + cmd->src0) - cmd->immediate;
+		strcpy(exeSrt, "EXEC: R[%d] = %d - %d", cmd->dst, cmd->src0, cmd->immediate);
 	}
-	if (cmd.src1 == 1 && cmd.src0 == 1)
+	if (cmd->src1 == 1 && cmd->src0 == 1)
 	{
-		*(procimg + cmd.dst) = cmd.immediate + cmd.immediate;
-		strcpy(exeSrt, "EXEC: R[%d] = %d + %d", cmd.dst, cmd.immediate, cmd.immediate);
+		*(procimg + cmd->dst) = cmd->immediate - cmd->immediate;
+		strcpy(exeSrt, "EXEC: R[%d] = %d - %d", cmd->dst, cmd->immediate, cmd->immediate);
 	}
-	if (cmd.src1 != 1 && cmd.src0 == 1)
+	if (cmd->src1 != 1 && cmd->src0 == 1)
 	{
-		*(procimg + cmd.dst) = cmd.immediate + *(procimg + cmd.src1);
-		strcpy(exeSrt, "EXEC: R[%d] = %d + %d", cmd.dst, cmd.src1, cmd.immediate);
+		*(procimg + cmd->dst) = cmd->immediate - *(procimg + cmd->src1);
+		strcpy(exeSrt, "EXEC: R[%d] = %d - %d", cmd->dst, cmd->immediate, cmd->src1);
 	}
-	if (cmd.src1 != 1 && cmd.src0 != 1)
+	if (cmd->src1 != 1 && cmd->src0 != 1)
 	{
-		*(procimg + cmd.dst) = *(procimg + cmd.src0) + *(procimg + cmd.src1);
-		strcpy(exeSrt, "EXEC: R[%d] = %d + %d", cmd.dst, cmd.src0, cmd.src0);
+		*(procimg + cmd->dst) = *(procimg + cmd->src0) - *(procimg + cmd->src1);
+		strcpy(exeSrt, "EXEC: R[%d] = %d - %d", cmd->dst, cmd->src0, cmd->src1);
+	}
+}
+
+void LsfFunc(int *procimg, Instruction *cmd, char *exeSrt)
+{
+	if (cmd->src1 == 1 && cmd->src0 != 1)
+	{
+		*(procimg + cmd->dst) = *(procimg + cmd->src0) << cmd->immediate;
+		strcpy(exeSrt, "EXEC: R[%d] = %d << %d", cmd->dst, cmd->src0, cmd->immediate);
+	}
+	if (cmd->src1 == 1 && cmd->src0 == 1)
+	{
+		*(procimg + cmd->dst) = cmd->immediate << cmd->immediate;
+		strcpy(exeSrt, "EXEC: R[%d] = %d << %d", cmd->dst, cmd->immediate, cmd->immediate);
+	}
+	if (cmd->src1 != 1 && cmd->src0 == 1)
+	{
+		*(procimg + cmd->dst) = cmd->immediate << *(procimg + cmd->src1);
+		strcpy(exeSrt, "EXEC: R[%d] = %d << %d", cmd->dst, cmd->immediate, cmd->src1);
+	}
+	if (cmd->src1 != 1 && cmd->src0 != 1)
+	{
+		*(procimg + cmd->dst) = *(procimg + cmd->src0) << *(procimg + cmd->src1);
+		strcpy(exeSrt, "EXEC: R[%d] = %d << %d", cmd->dst, cmd->src0, cmd->src1);
+	}
+}
+
+void RsfFunc(int *procimg, Instruction *cmd, char *exeSrt)
+{
+	if (cmd->src1 == 1 && cmd->src0 != 1)
+	{
+		*(procimg + cmd->dst) = *(procimg + cmd->src0) >> cmd->immediate;
+		strcpy(exeSrt, "EXEC: R[%d] = %d >> %d", cmd->dst, cmd->src0, cmd->immediate);
+	}
+	if (cmd->src1 == 1 && cmd->src0 == 1)
+	{
+		*(procimg + cmd->dst) = cmd->immediate >> cmd->immediate;
+		strcpy(exeSrt, "EXEC: R[%d] = %d >> %d", cmd->dst, cmd->immediate, cmd->immediate);
+	}
+	if (cmd->src1 != 1 && cmd->src0 == 1)
+	{
+		*(procimg + cmd->dst) = cmd->immediate >> *(procimg + cmd->src1);
+		strcpy(exeSrt, "EXEC: R[%d] = %d >> %d", cmd->dst, cmd->immediate, cmd->src1);
+	}
+	if (cmd->src1 != 1 && cmd->src0 != 1)
+	{
+		*(procimg + cmd->dst) = *(procimg + cmd->src0) >> *(procimg + cmd->src1);
+		strcpy(exeSrt, "EXEC: R[%d] = %d >> %d", cmd->dst, cmd->src0, cmd->src1);
+	}
+}
+
+void AndFunc(int *procimg, Instruction *cmd, char *exeSrt)
+{
+	if (cmd->src1 == 1 && cmd->src0 != 1)
+	{
+		*(procimg + cmd->dst) = *(procimg + cmd->src0) & cmd->immediate;
+		strcpy(exeSrt, "EXEC: R[%d] = %d & %d", cmd->dst, cmd->src0, cmd->immediate);
+	}
+	if (cmd->src1 == 1 && cmd->src0 == 1)
+	{
+		*(procimg + cmd->dst) = cmd->immediate & cmd->immediate;
+		strcpy(exeSrt, "EXEC: R[%d] = %d & %d", cmd->dst, cmd->immediate, cmd->immediate);
+	}
+	if (cmd->src1 != 1 && cmd->src0 == 1)
+	{
+		*(procimg + cmd->dst) = cmd->immediate & *(procimg + cmd->src1);
+		strcpy(exeSrt, "EXEC: R[%d] = %d & %d", cmd->dst, cmd->immediate, cmd->src1);
+	}
+	if (cmd->src1 != 1 && cmd->src0 != 1)
+	{
+		*(procimg + cmd->dst) = *(procimg + cmd->src0) & *(procimg + cmd->src1);
+		strcpy(exeSrt, "EXEC: R[%d] = %d & %d", cmd->dst, cmd->src0, cmd->src1);
+	}
+}
+
+void OrFunc(int *procimg, Instruction *cmd, char *exeSrt)
+{
+	if (cmd->src1 == 1 && cmd->src0 != 1)
+	{
+		*(procimg + cmd->dst) = *(procimg + cmd->src0) | cmd->immediate;
+		strcpy(exeSrt, "EXEC: R[%d] = %d | %d", cmd->dst, cmd->src0, cmd->immediate);
+	}
+	if (cmd->src1 == 1 && cmd->src0 == 1)
+	{
+		*(procimg + cmd->dst) = cmd->immediate | cmd->immediate;
+		strcpy(exeSrt, "EXEC: R[%d] = %d | %d", cmd->dst, cmd->immediate, cmd->immediate);
+	}
+	if (cmd->src1 != 1 && cmd->src0 == 1)
+	{
+		*(procimg + cmd->dst) = cmd->immediate | *(procimg + cmd->src1);
+		strcpy(exeSrt, "EXEC: R[%d] = %d | %d", cmd->dst, cmd->immediate, cmd->src1);
+	}
+	if (cmd->src1 != 1 && cmd->src0 != 1)
+	{
+		*(procimg + cmd->dst) = *(procimg + cmd->src0) | *(procimg + cmd->src1);
+		strcpy(exeSrt, "EXEC: R[%d] = %d | %d", cmd->dst, cmd->src0, cmd->src1);
+	}
+}
+
+void OrFunc(int *procimg, Instruction *cmd, char *exeSrt)
+{
+	if (cmd->src1 == 1 && cmd->src0 != 1)
+	{
+		*(procimg + cmd->dst) = *(procimg + cmd->src0) ^ cmd->immediate;
+		strcpy(exeSrt, "EXEC: R[%d] = %d ^ %d", cmd->dst, cmd->src0, cmd->immediate);
+	}
+	if (cmd->src1 == 1 && cmd->src0 == 1)
+	{
+		*(procimg + cmd->dst) = cmd->immediate ^ cmd->immediate;
+		strcpy(exeSrt, "EXEC: R[%d] = %d ^ %d", cmd->dst, cmd->immediate, cmd->immediate);
+	}
+	if (cmd->src1 != 1 && cmd->src0 == 1)
+	{
+		*(procimg + cmd->dst) = cmd->immediate ^ *(procimg + cmd->src1);
+		strcpy(exeSrt, "EXEC: R[%d] = %d ^ %d", cmd->dst, cmd->immediate, cmd->src1);
+	}
+	if (cmd->src1 != 1 && cmd->src0 != 1)
+	{
+		*(procimg + cmd->dst) = *(procimg + cmd->src0) ^ *(procimg + cmd->src1);
+		strcpy(exeSrt, "EXEC: R[%d] = %d ^ %d", cmd->dst, cmd->src0, cmd->src1);
 	}
 }
